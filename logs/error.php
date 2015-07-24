9ad2d92de5ad44462a7505ebd9ce961b8eb0bebb
Rex Bengil <centraleffects@yahoo.com>
2015-07-24 21:15:55 +0800

    Original Files from live site

---
+++ logs/error.php
@@ -0,0 +1,19 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2015-05-20 02:10:41 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2015-05-20T02:10:41+00:00	INFO 124.187.116.21	joomlafailure	Username and password do not match or you do not have an account yet.
+2015-05-21T21:22:58+00:00	INFO 109.236.90.133	joomlafailure	Username and password do not match or you do not have an account yet.
+2015-05-22T05:31:17+00:00	INFO 124.187.116.21	joomlafailure	Username and password do not match or you do not have an account yet.
+2015-05-22T10:06:32+00:00	INFO 188.163.91.211	joomlafailure	Username and password do not match or you do not have an account yet.
+2015-05-23T07:36:27+00:00	INFO 109.236.90.133	joomlafailure	Username and password do not match or you do not have an account yet.
+2015-05-23T12:36:09+00:00	INFO 188.163.91.26	joomlafailure	Username and password do not match or you do not have an account yet.
+2015-05-24T01:54:31+00:00	INFO 109.236.90.133	joomlafailure	Username and password do not match or you do not have an account yet.
+2015-05-24T08:44:34+00:00	INFO 188.163.88.127	joomlafailure	Username and password do not match or you do not have an account yet.
+2015-05-25T00:14:52+00:00	INFO 188.163.88.127	joomlafailure	Username and password do not match or you do not have an account yet.
+2015-05-25T05:54:53+00:00	INFO 77.243.189.206	joomlafailure	Username and password do not match or you do not have an account yet.
+2015-05-25T23:01:19+00:00	INFO 188.163.88.127	joomlafailure	Username and password do not match or you do not have an account yet.
+2015-05-26T00:19:57+00:00	INFO 77.243.189.206	joomlafailure	Username and password do not match or you do not have an account yet.
+2015-05-26T21:10:59+00:00	INFO 188.163.88.167	joomlafailure	Username and password do not match or you do not have an account yet.
