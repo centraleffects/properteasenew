fd026371c59ccde36a915ea838e6d57b41d35864
Rex Bengil <centraleffects@yahoo.com>
2015-08-05 12:03:19 +0800

    Update UI: Added font icons

---
+++ templates/propertease/js/script.js
@@ -0,0 +1,55 @@
+ //<![CDATA[  
+if (navigator.userAgent.match(/IEMobile\/10\.0/)) {  
+    var msViewportStyle = document.createElement("style");  
+    msViewportStyle.appendChild(  
+         document.createTextNode("@-ms-viewport{width:auto!important}")  
+    );  
+    document.getElementsByTagName("head")[0].appendChild(msViewportStyle);  
+}  
+//]]> 
+jQuery(window).on('load', function() {  
+                     new JCaption('img.caption');  
+                });  
+ jQuery(document).ready(function(){  
+      jQuery('.hasTooltip').tooltip({"html": true,"container": "body"});  
+ }); 
+jQuery(document).ready(function($){
+	Joomla.JText.load({"REQUIRED_FILL_ALL":"Please enter data in all fields.","E_LOGIN_AUTHENTICATE":"Username and password do not match or you do not have an account yet.","REQUIRED_NAME":"Please enter your name!","REQUIRED_USERNAME":"Please enter your username!","REQUIRED_PASSWORD":"Please enter your password!","REQUIRED_VERIFY_PASSWORD":"Please re-enter your password!","PASSWORD_NOT_MATCH":"Password does not match the verify password!","REQUIRED_EMAIL":"Please enter your email!","EMAIL_INVALID":"Please enter a valid email!","REQUIRED_VERIFY_EMAIL":"Please re-enter your email!","EMAIL_NOT_MATCH":"Email does not match the verify email!","CAPTCHA_REQUIRED":"Please enter captcha key"});  
+
+	 $('.selectpicker').selectpicker();
+	$('#report-name').focus(function(){
+		var e = $(this);
+		if(e.val()=="Give this report a name"){e.val(''); }
+	}).focusout(function(){
+		var e = $(this);
+		if(e.val()==""){e.val('Give this report a name'); $('.step-process li').removeClass('active'); }
+		else{
+			$('.step-process ul li:nth-child(1)').addClass('active');
+		}
+	});
+
+	$('.state-n').change(function(){
+		$('#state').val($(this).val());
+		get_council();
+		$('.step-process ul li:nth-child(2)').addClass('active');
+		$(this).parent('span').parent('a').addClass('filled-step');
+	});
+	$('.council-n').change(function(){
+		$('#council').val($(this).val());
+		get_scheme();
+		$('.step-process ul li:nth-child(3)').addClass('active');
+		$(this).parent('span').parent('a').addClass('filled-step');
+	});
+	$('.scheme-n').change(function(){
+		$('#scheme').val($(this).val());
+		get_restriction();
+		$('.step-process ul li:nth-child(4)').addClass('active');
+		$(this).parent('span').parent('a').addClass('filled-step');
+	});
+	$('.zone-n').change(function(){
+		$('#zone').val($(this).val());
+		get_submit();
+		$('.step-process ul li:nth-child(5)').addClass('active');
+		$(this).parent('span').parent('a').addClass('filled-step');
+	});
+});
