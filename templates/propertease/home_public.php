d939e22d3691d077fa3579402052535886aad484
fred <nelfred214@yahoo.com.ph>
2015-08-10 12:33:35 +0800

    remove misplaced text

--- templates/propertease/home_public.php
+++ templates/propertease/home_public.php
@@ -243,14 +243,6 @@ jQuery(function() {
                 <p>One of our town planning clients saves up to 2 hours a day between 3 staff by utilizing the PropertEASE service.</p>
              </div><!-- tab pane -->
              
-          The PropertEASE online system can provide you with instant answers when discussing a property with a prospective seller or purchaser. A month to month or yearly unlimited subscription can ensure that you get the best value for money to utilize the system. PropertEASE provides:
-
-    Easy instant information (using our online resources);
-    A simple overview of a properties development potential to give you an informed sales pitch and the upper hand in negotiations;
-    Ensures that your asking price coincides with a properties potential; I.e. get the best sales price with the correct information;
-    Saves hours a day waiting on hold to Council or town planners to get the same answers; and
-    Is mobile friendly (i.e. use it on site).
-   
              <div id="real-estate-agents" class="tab-pane fade in"><!-- tab pane -->
                 <h1 style="font-size: 37px;">Benefits for Real Estate Agents</h1>
                 <p class="med" style="font-size: 20px;">The PropertEASE online system can provide you with instant answers when discussing a property with a prospective seller or purchaser.</p>
